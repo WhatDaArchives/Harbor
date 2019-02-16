380ce70dfb1cabaf01de3e1d38a5664a7e436b13
Valentin Prugnaud <valentin@whatdafox.com>
2019-02-16 07:30:47 -0800

    Add dependency for mysql and redis, update php image, add test container

--- app/Builders/DockerComposeBuilder.php
+++ app/Builders/DockerComposeBuilder.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Builders;
-
 
 use Spyc;
 
@@ -14,30 +12,32 @@ class DockerComposeBuilder
     private $contents = [
         'version' => '3',
         'services' => [
-            'nginx' => [
-                'image' => 'whatdafox/nginx-php:latest',
+            'app' => [
+                'image' => 'webdevops/php-nginx:7.3',
                 'volumes' => [
-                    '.:/var/www/html',
+                    '.:/var/www/html:delegated',
                 ],
                 'ports' => [
-                    '${APP_PORT:-80}:80',
+                    '${APP_PORT}:80',
+                    '${APP_SSL_PORT}:443',
                 ],
                 'networks' => [
                     'app-net',
                 ],
+                'environment' => [
+                    'WEB_DOCUMENT_ROOT' => '/var/www/html/public',
+                ],
+                'restart' => 'on-failure'
             ],
-            'app' => [
-                'image' => 'whatdafox/php-fpm:latest',
+            'test' => [
+                'image' => 'webdevops/php:7.3',
                 'volumes' => [
-                    '.:/var/www/html',
-                ],
-                'env_file' => [
-                    '.env',
+                    '.:/var/www/html'
                 ],
                 'networks' => [
                     'app-net',
-                ],
-            ],
+                ]
+            ]
         ],
         'networks' => [
             'app-net' => [
@@ -70,7 +70,16 @@ class DockerComposeBuilder
             'networks' => [
                 'app-net',
             ],
+            'restart' => 'on-failure',
+            'environment' => [
+                'MYSQL_ROOT_PASSWORD' => "\${DB_ROOT_PASS}",
+                'MYSQL_DATABASE' => "\${DB_DATABASE}",
+                'MYSQL_USER' => "\${DB_USERNAME}",
+                'MYSQL_PASSWORD' => "\${DB_PASSWORD}",
+            ]
         ];
+
+        $this->addDependency('mysql');
 
         return $this;
     }
@@ -85,13 +94,19 @@ class DockerComposeBuilder
             'volumes' => [
                 'redisdata:/data',
             ],
+            'restart' => 'on-failure',
             'ports' => [
                 '${REDIS_PORT:-6379}:6379',
+            ],
+            'environment' => [
+                'REDIS_PORT' => '${REDIS_PORT}:6379',
             ],
             'networks' => [
                 'app-net',
             ],
         ];
+
+        $this->addDependency('redis');
 
         return $this;
     }
@@ -102,5 +117,22 @@ class DockerComposeBuilder
     public function __toString()
     {
         return Spyc::YAMLDump($this->contents, false, false, true);
+    }
+
+    /**
+     *
+     */
+    private function addDependency($dependency): void
+    {
+        if (!array_key_exists('depends_on', $this->contents['services']['app'])) {
+            $this->contents['services']['app']['depends_on'] = [];
+        }
+
+        if (!array_key_exists('depends_on', $this->contents['services']['test'])) {
+            $this->contents['services']['test']['depends_on'] = [];
+        }
+
+        array_push($this->contents['services']['app']['depends_on'], $dependency);
+        array_push($this->contents['services']['test']['depends_on'], $dependency);
     }
 }
