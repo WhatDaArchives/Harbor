9744b30838d60da48bd8aed3e2fab48b7ec61d00
Valentin Prugnaud <valentin@whatdafox.com>
2019-02-16 07:54:34 -0800

    Improve option capturing, boost PHP version to 7.3

--- app/Commands/InitDocker.php
+++ app/Commands/InitDocker.php
@@ -23,7 +23,7 @@ class InitDocker extends Command
      *
      * @var string
      */
-    protected $signature = 'init {--force : Force initialization}';
+    protected $signature = 'init {--force : Force initialization} {--mysql : Add MySQL database configuration (true/false)} {--redis : Add Redis configuration (true/false)}';
 
     /**
      * The description of the command.
@@ -50,8 +50,7 @@ class InitDocker extends Command
             $this->filename = "docker-compose.{$this->option('env')}.yml";
         }
 
-        $this->settings['mysql'] = $this->confirm('Will you be using a database?', true);
-        $this->settings['redis'] = $this->confirm('Will you be using a redis?', true);
+        $this->parseOptionsOrAsk();
 
         // Generate docker-compose
         $this->info("Generate {$this->filename}...");
@@ -83,5 +82,32 @@ class InitDocker extends Command
     private function writeFile(string $name, string $contents): void
     {
         file_put_contents(getcwd() . '/' . $name, $contents);
+    }
+
+    /**
+     * @return void
+     */
+    private function parseOptionsOrAsk(): void
+    {
+        $this->settings['mysql'] = $this->getOption('mysql', 'Will you be using MySQL?');
+        $this->settings['redis'] = $this->getOption('redis', 'Will you be using Redis?');
+    }
+
+    /**
+     * @param $option
+     * @param $question
+     * @param bool $default
+     * @return bool
+     */
+    private function getOption($option, $question, $default = true): bool {
+        if (!$this->option($option)) {
+            if(!$this->option('no-interaction')) {
+                return (bool) $this->confirm($question, $default);
+            }
+
+            return false;
+        }
+
+        return (bool) $this->option($option);
     }
 }
